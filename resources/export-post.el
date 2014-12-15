1aac9fb3c0f59e7c55ef31579a7f5fdb05a3fab6
ryang <ryang.sei@gmail.com>
2014-12-15 22:56:23 +1100

    modify emacs lisp script to export html. fix one minor issue, should use fboundp to test if function symbol is void or not

--- resources/export-post.el
+++ resources/export-post.el
@@ -2,11 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 (progn
-  (load "~/.emacs.d/custom.el")
-  (package-initialize)
-  (require 'cl)
-  (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-  (load-theme 'zenburn t)
   (require 'org)
   (find-file (car (last command-line-args)))
   (setq org-confirm-babel-evaluate nil)
